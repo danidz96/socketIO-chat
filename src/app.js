c5e8966697593c1d086ff0d9753b78baef55610c
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-21 21:18:57 +0100

    initial setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
 
 module.exports = app;
