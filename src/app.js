d27f0a43ebf65df5c0cd9daf7be560f76c8abfc4
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-22 21:28:25 +0100

    setup socket.io

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
+io.on('connection', () => {
+	console.log('New WebSocket connection');
+});
 
 module.exports = app;
