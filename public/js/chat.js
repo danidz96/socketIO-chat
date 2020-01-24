2fd6af23f1d613bf0d6c26995acf871f290902b4
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-24 15:45:44 +0100

    add message form

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,5 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-	console.log('The count has been updated!', count);
-});
-
-document.querySelector('#increment').addEventListener('click', () => {
-	console.log('Clicked');
-	socket.emit('increment');
+socket.on('message', (message) => {
+	console.log(message);
 });
--- src/index.js
+++ src/index.js
@@ -17,11 +17,7 @@ let count = 0;
 io.on('connection', (socket) => {
 	console.log('New WebSocket connection');
 
-	socket.emit('countUpdated', count);
-	socket.on('increment', () => {
-		count++;
-		io.emit('countUpdated', count);
-	});
+	socket.emit('message', 'Welcome!');
 });
 
 server.listen(port, () => {
