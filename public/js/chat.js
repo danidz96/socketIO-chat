a36a5aab3df89961ede70e0f6f5197c7faef2cd8
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-29 20:29:09 +0100

    add profanity filter

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,7 +9,12 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
 	const message = e.target.elements.message.value;
 
-	socket.emit('sendMessage', message);
+	socket.emit('sendMessage', message, (error) => {
+		if (error) {
+			return console.log(error);
+		}
+		console.log('Message delivered!');
+	});
 });
 
 document.querySelector('#send-location').addEventListener('click', () => {
