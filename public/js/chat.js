5f6a0e8e8ba2ffba5bdb5dffccfa10dd91984d05
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-01-31 19:51:29 +0100

    render messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,9 +4,17 @@ const messageForm = document.querySelector('#message-form');
 const messageFormInput = messageForm.querySelector('input');
 const messageFormButton = messageForm.querySelector('button');
 const sendLocationButton = document.querySelector('#send-location');
+const messages = document.querySelector('#messages');
+
+const messageTemplate = document.querySelector('#message-template').innerHTML;
 
 socket.on('message', (message) => {
 	console.log(message);
+
+	const html = Mustache.render(messageTemplate, {
+		message
+	});
+	messages.insertAdjacentHTML('beforeend', html);
 });
 
 document.querySelector('#message-form').addEventListener('submit', (e) => {
