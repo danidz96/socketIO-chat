8540920ea31c0c5ea7ad23acf7ae2c12aff38295
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-02-02 15:45:26 +0100

    print url with location

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,6 +7,7 @@ const sendLocationButton = document.querySelector('#send-location');
 const messages = document.querySelector('#messages');
 
 const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
 
 socket.on('message', (message) => {
 	console.log(message);
@@ -14,6 +15,11 @@ socket.on('message', (message) => {
 	const html = Mustache.render(messageTemplate, {
 		message
 	});
+	messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on('locationMessage', (url) => {
+	const html = Mustache.render(locationMessageTemplate, { url });
 	messages.insertAdjacentHTML('beforeend', html);
 });
 
